ef8662fdee359135c9b45be9828fbabd95db40dc
lexieastman <123565264+lexieastman@users.noreply.github.com>
2023-03-16 14:56:26 -0400

    Update Line.py

--- Project1/Line.py
+++ Project1/Line.py
@@ -7,9 +7,11 @@ class Line:
         self.__start=start
         self.__end=end
 
-    def validLine(self,l2): 
+    def validLine(self): 
         if self.__start[0]!=self.__end[0] and self.__start[1]!=self.__end[1]:
             return False
+        else:
+            return True
     
     def getStart(self):
         return self.__start
@@ -20,4 +22,12 @@ class Line:
     def printLine(self):
         print(self.__start,self.__end)
 
+    def equals(self,l2):
+        if self.__start==l2.__start & self.__end==l2.__end:
+            return True
+        else:
+            return False
+    
+    def list(self):
+        return [self.__start[0],self.__start[1],self.__end[0],self.__end[1]]
 
