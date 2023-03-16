af1ec4f630d1df07ee4ac249628b07485101e6eb
lexieastman <123565264+lexieastman@users.noreply.github.com>
2023-03-16 14:55:59 -0400

    Update App.py

--- Project1/App.py
+++ Project1/App.py
@@ -3,13 +3,14 @@ from Line import Line
 from Table import Table
 
 lines=[]
-player1= ""
-player2= ""
+player1= 0
+player2= 0
 turn = player1
 lines=[]
 
-def createTable():
-    t=Table(4,4)
+
+def createTable(x,y):
+    t=Table(x,y)
     t.constructTable()
     print("reference coordinates: ")
     t.printCoordinates()
@@ -26,8 +27,7 @@ def newLine():
     print("Enter horizontal coordinate for line end point: ")
     b=int(input())
     line=Line([x,y],[a,b])
-    lines.append ([x,y])
-    lines.append ([a,b])
+    
     
     return line
 
@@ -40,18 +40,75 @@ def updateTable(line,table):
         if line.getEnd()[0]==table.getValue(i)[0] and line.getEnd()[1]==table.getValue(i)[1]:
             table.changeSymbol(i,"X")
 
-##def square(l):
 
-##def turn():
+    
+def used(line):
+    for i in range (len(lines)):
+        if line.list()==lines[i]:
+            flag= True
+        else: 
+            lines.append(line)
+            flag=False
+        return flag
 
+def turn(player,table):
+    x=newLine()
+    print(x.validLine())
+    
+    while used(x)==True:
+        x=newLine()
+        print(x.printLine())
+        print("Enter a valid line ")
+    
+    updateTable(x,table)
+    
+    table.printTable()
+
+   
+def endGame(table):
+    count=0
+    end=False
+    size=table.getSize()
+    for i in range (size):
+        if table.getValue(i)=="X":
+            count+=1
+
+    if count==size:
+        end=True
+
+    return end
+
+
+            
 
 def main():
-    table= createTable()
-    table.printTable()
-    line=newLine()
-    updateTable(line,table)
-    table.printTable()
-    print(lines)
+    print("hello")
+    table=createTable(4,4)
+    print(table.getSize())
+    t =0
+    end=False
+   
+   
+
+    while (endGame(table)==False):
+        if t%2==0:
+            p=player1
+            print("Player 1 turn: ")
+        else:
+            p=player2
+            print("player 2 turn: ")
+        
+        turn(p,table)
+        t+=1
+   
+
+    print("Score: ")
+    print("Player 1: "+str(player1))
+    print ("Player 2: "+str(player2))
+        
+
+    
+    
     
 
 
